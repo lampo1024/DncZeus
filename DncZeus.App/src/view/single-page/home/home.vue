2af87057abec73454f232b68f5ceacbb6d8490db
lampo1024 <lampo1024@gmail.com>
2018-12-22 23:32:58 +0800

    Change home page row gutter value

--- DncZeus.App/src/view/single-page/home/home.vue
+++ DncZeus.App/src/view/single-page/home/home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Row :gutter="20">
+    <Row :gutter="10">
       <i-col :xs="12" :md="8" :lg="4" v-for="(infor, i) in inforCardData" :key="`infor-${i}`" style="height: 120px;padding-bottom: 10px;">
         <infor-card shadow :color="infor.color" :icon="infor.icon" :icon-size="36">
           <count-to :end="infor.count" count-class="count-style"/>
@@ -8,7 +8,7 @@
         </infor-card>
       </i-col>
     </Row>
-    <Row :gutter="20" style="margin-top: 10px;">
+    <Row :gutter="10" style="margin-top: 10px;">
       <i-col :md="24" :lg="8" style="margin-bottom: 20px;">
         <Card shadow>
           <chart-pie style="height: 300px;" :value="pieData" text="用户访问来源"></chart-pie>
