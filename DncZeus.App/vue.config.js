dea3e42e46b979e5a023411e42a0ef3cb10cea62
lampo1024 <lampo1024@gmail.com>
2018-12-22 22:46:27 +0800

    Add disableHostCheck option in vue.config.js

--- DncZeus.App/vue.config.js
+++ DncZeus.App/vue.config.js
@@ -36,7 +36,9 @@ module.exports = {
   productionSourceMap: false,
   // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
   devServer: {
-  // proxy: 'localhost:3000'
-	port: 9000
+    // proxy: 'localhost:3000'
+    port: 9000,
+    //禁用host检查,不然在部分流利器中会报[WDS disconnected]的错误
+    disableHostCheck: true
   }
 }
